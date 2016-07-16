02c87595cf17a69e9914eb0a06f0849bb85e5a55
Njeri Mbugua <jemynj@gmail.com>
2016-07-16 14:24:55 +0300

    add student details to dictionary and print values

--- students.py
+++ students.py
@@ -1,1 +1,13 @@
-students ={}
+from datetime import date
+
+students ={
+	'name':['Njeri', 'Mbugua'],
+	'date_of_birth':1992,
+	'phone':'073433433',
+}
+print('the first name is {0} and the second name is {1}'.format(students.get('name')[0], students.get('name')[1]))
+print(students['name'][0])
+print(students['phone'])
+print(students['date_of_birth'])
+age = date.today().year - students['date_of_birth']
+print(age)
