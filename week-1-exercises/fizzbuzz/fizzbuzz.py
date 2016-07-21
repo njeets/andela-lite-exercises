f09b98933daabfd0a9ed362a0d1e364969311e55
Njeri Mbugua <jemynj@gmail.com>
2016-07-21 11:41:47 +0300

    fizzbuzz function

---
+++ week-1-exercises/fizzbuzz/fizzbuzz.py
@@ -0,0 +1,14 @@
+
+
+def fizzbuzz(n):
+
+    if n % 3 == 0 and n % 5 == 0:
+        return 'FizzBuzz'
+    elif n % 3 == 0:
+        return 'Fizz'
+    elif n % 5 == 0:
+        return 'Buzz'
+    else:
+        return n
+
+print fizzbuzz(66)
