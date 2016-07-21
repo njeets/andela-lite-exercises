fabc19fd295eac7a3f6c3742935b3b924d7acce8
Njeri Mbugua <jemynj@gmail.com>
2016-07-21 13:01:29 +0300

    'factorial'

---
+++ week-1-exercises/factorial/factorial.py
@@ -0,0 +1,22 @@
+#brief notes
+# n = 3
+#
+
+def factorial(n):
+	result = 1
+
+	#loops through the numbers
+	for i in range(1, n+1):
+		result *= i
+
+	# this displays the result	
+	return result 
+
+
+
+print factorial(6)
+
+
+
+		
+	
